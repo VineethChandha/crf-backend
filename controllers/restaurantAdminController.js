daa74eebe202b66f319d6682696db35384d37221
pankaj3399 <vatwanipankaj74@gmail.com>
2024-08-08 22:06:47 +0530

    Email try

--- controllers/restaurantAdminController.js
+++ controllers/restaurantAdminController.js
@@ -179,15 +179,41 @@ export const addReward = async (req, res) => {
       text: text,
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send("Error sending email");
-      } else {
-        console.log("Email sent: " + info.response);
-        res.send("Email sent successfully");
-      }
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
     });
+
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
+
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     console.log(error);
+    //     res.status(500).send("Error sending email");
+    //   } else {
+    //     console.log("Email sent: " + info.response);
+    //     res.send("Email sent successfully");
+    //   }
+    // });
 
     return res.status(200).json({
       message:
